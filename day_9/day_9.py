3631b1cc88d3eee603202709f1d5002d0d5ba7c1
Atix95 <michael.weide08@gmail.com>
2022-03-04 20:12:37 +0100

    Solved day 9

--- day_9/day_9.py
+++ day_9/day_9.py
@@ -18,11 +18,10 @@ def load_heightmap(file_name):
 
 def find_lowpoint(heightmap):
     sum_of_lowpoints = 0
-    debug = []
 
     for v_index, line in enumerate(heightmap):
         for h_index, digit in enumerate(line):
-            path = []
+
             if (
                 digit[1] == False
                 and h_index < len(line) - 1
@@ -33,7 +32,6 @@ def find_lowpoint(heightmap):
                         digit[1] = True
                         h_index += 1
                         digit = line[h_index]
-                        path.append(digit[0])
                         if h_index + 2 >= len(line):
                             break
                     else:
@@ -46,165 +44,96 @@ def find_lowpoint(heightmap):
                             digit[1] = True
                             v_run_variable += 1
                             digit = heightmap[v_run_variable][h_index]
-                            path.append(digit[0])
                             if v_run_variable + 2 >= len(heightmap):
                                 break
                         else:
                             break
-            elif (
-                digit[1] == False
-                and h_index < len(line) - 1
-                and v_index >= len(heightmap) - 1
-            ):
-                while digit[0] > line[h_index + 1][0]:
-                    if line[h_index + 1][1] == False:
-                        digit[1] = True
-                        h_index += 1
-                        digit = line[h_index]
-                        path.append(digit[0])
-                        if h_index + 2 >= len(line):
-                            break
-                    else:
-                        break
-                v_run_variable = v_index
-            elif (
-                digit[1] == False
-                and h_index >= len(line) - 1
-                and v_index < len(heightmap) - 1
-            ):
-                v_run_variable = v_index
-                if v_run_variable < len(heightmap):
-                    while digit[0] > heightmap[v_run_variable + 1][h_index][0]:
-                        if heightmap[v_run_variable + 1][h_index][1] == False:
-                            digit[1] = True
-                            v_run_variable += 1
-                            digit = heightmap[v_run_variable][h_index]
-                            path.append(digit[0])
-                            if v_run_variable + 2 >= len(heightmap):
-                                break
-                        else:
-                            break
+
             # Cases to check for possible lowpoint depending on location in heightmap:
-            # Middle
-            # Top line
-            # Bottom line
-            # Left line
-            # Right line
-            # Top left corner
-            # Top right corner
-            # Bottom left corner
-            # Bottom right corner
+            # Middle, Top line, Bottom line, Left line, Right line,
+            # Top left corner, Top right corner, Bottom left corner, Bottom right corner
+            if digit[1] == False:
+                if (
+                    0 < h_index < len(line) - 1
+                    and 0 < v_run_variable < len(heightmap) - 1
+                ):
+                    # Middle of the heightmap -> check in all directions
+                    if (
+                        digit[0] < heightmap[v_run_variable][h_index + 1][0]
+                        and digit[0] < heightmap[v_run_variable][h_index - 1][0]
+                        and digit[0] < heightmap[v_run_variable + 1][h_index][0]
+                        and digit[0] < heightmap[v_run_variable - 1][h_index][0]
+                    ):
+                        sum_of_lowpoints += digit[0] + 1
+                elif 0 < h_index < len(line) - 1 and v_run_variable <= 0:
+                    # Top line of the heightmap -> check to the left, right and bottom
+                    if (
+                        digit[0] < heightmap[v_run_variable][h_index - 1][0]
+                        and digit[0] < heightmap[v_run_variable][h_index + 1][0]
+                        and digit[0] < heightmap[v_run_variable + 1][h_index][0]
+                    ):
+                        sum_of_lowpoints += digit[0] + 1
+                elif (
+                    0 < h_index < len(line) - 1 and v_run_variable >= len(heightmap) - 1
+                ):
+                    # Bottom line of the heightmap -> check to the left, right and top
+                    if (
+                        digit[0] < heightmap[v_run_variable][h_index - 1][0]
+                        and digit[0] < heightmap[v_run_variable][h_index + 1][0]
+                        and digit[0] < heightmap[v_run_variable - 1][h_index][0]
+                    ):
+                        sum_of_lowpoints += digit[0] + 1
+                elif h_index <= 0 and 0 < v_run_variable < len(heightmap) - 1:
+                    # Left line of the heightmap -> check to the right, top and bottom
+                    if (
+                        digit[0] < heightmap[v_run_variable][h_index + 1][0]
+                        and digit[0] < heightmap[v_run_variable - 1][h_index][0]
+                        and digit[0] < heightmap[v_run_variable + 1][h_index][0]
+                    ):
+                        sum_of_lowpoints += digit[0] + 1
+                elif (
+                    h_index >= len(line) - 1 and 0 < v_run_variable < len(heightmap) - 1
+                ):
+                    # Right line of the heightmap -> check to the left, top and bottom
+                    if (
+                        digit[0] < heightmap[v_run_variable][h_index - 1][0]
+                        and digit[0] < heightmap[v_run_variable - 1][h_index][0]
+                        and digit[0] < heightmap[v_run_variable + 1][h_index][0]
+                    ):
+                        sum_of_lowpoints += digit[0] + 1
+                elif h_index <= 0 and v_run_variable <= 0:
+                    # Top left corner of the heightmap -> check to right and bottom
+                    if (
+                        digit[0] < heightmap[v_run_variable][h_index + 1][0]
+                        and digit[0] < heightmap[v_run_variable + 1][h_index][0]
+                    ):
+                        sum_of_lowpoints += digit[0] + 1
+                elif h_index >= len(line) - 1 and v_run_variable <= 0:
+                    # Top right corner of the heightmap -> check to left and bottom
+                    if (
+                        digit[0] < heightmap[v_run_variable][h_index - 1][0]
+                        and digit[0] < heightmap[v_run_variable + 1][h_index][0]
+                    ):
+                        sum_of_lowpoints += digit[0] + 1
+                elif h_index <= 0 and v_run_variable >= len(heightmap) - 1:
+                    # Bottom left corner of the heighmap -> check to right and top
+                    if (
+                        digit[0] < heightmap[v_run_variable][h_index + 1][0]
+                        and digit[0] < heightmap[v_run_variable - 1][h_index][0]
+                    ):
+                        sum_of_lowpoints += digit[0] + 1
+                elif h_index >= len(line) - 1 and v_run_variable >= len(heightmap) - 1:
+                    # Bottom right corner of the heighmap -> check to left and top
+                    if (
+                        digit[0] < heightmap[v_run_variable][h_index - 1][0]
+                        and digit[0] < heightmap[v_run_variable - 1][h_index][0]
+                    ):
+                        sum_of_lowpoints += digit[0] + 1
 
-            # print(h_index, v_run_variable)
-            if 0 < h_index < len(line) - 1 and 0 < v_run_variable < len(heightmap) - 1:
-                # Middle of the heightmap -> check in all directions
-                if (
-                    digit[0] < heightmap[v_run_variable][h_index + 1][0]
-                    and digit[0] < heightmap[v_run_variable][h_index - 1][0]
-                    and digit[0] < heightmap[v_run_variable + 1][h_index][0]
-                    and digit[0] < heightmap[v_run_variable - 1][h_index][0]
-                    and digit[1] == False
-                ):
-                    sum_of_lowpoints += digit[0] + 1
-                    digit[1] = True
-                    debug.append((digit[0], h_index, v_run_variable))
-                    print(path)
-            elif 0 < h_index < len(line) - 1 and v_run_variable <= 0:
-                # Top line of the heightmap -> check to the left, right and bottom
-                if (
-                    digit[0] < heightmap[v_run_variable][h_index - 1][0]
-                    and digit[0] < heightmap[v_run_variable][h_index + 1][0]
-                    and digit[0] < heightmap[v_run_variable + 1][h_index][0]
-                    and digit[1] == False
-                ):
-                    sum_of_lowpoints += digit[0] + 1
-                    digit[1] = True
-                    debug.append((digit[0], h_index, v_run_variable))
-                    print(path)
-            elif 0 < h_index < len(line) - 1 and v_run_variable >= len(heightmap) - 1:
-                # Bottom line of the heightmap -> check to the left, right and top
-                if (
-                    digit[0] < heightmap[v_run_variable][h_index - 1][0]
-                    and digit[0] < heightmap[v_run_variable][h_index + 1][0]
-                    and digit[0] < heightmap[v_run_variable - 1][h_index][0]
-                    and digit[1] == False
-                ):
-                    sum_of_lowpoints += digit[0] + 1
-                    digit[1] = True
-                    debug.append((digit[0], h_index, v_run_variable))
-                    print(path)
-            elif h_index <= 0 and 0 < v_run_variable < len(heightmap) - 1:
-                # Left line of the heightmap -> check to the right, top and bottom
-                if (
-                    digit[0] < heightmap[v_run_variable][h_index + 1][0]
-                    and digit[0] < heightmap[v_run_variable - 1][h_index][0]
-                    and digit[0] < heightmap[v_run_variable + 1][h_index][0]
-                    and digit[1] == False
-                ):
-                    sum_of_lowpoints += digit[0] + 1
-                    digit[1] = True
-                    debug.append((digit[0], h_index, v_run_variable))
-                    print(path)
-            elif h_index >= len(line) - 1 and 0 < v_run_variable < len(heightmap) - 1:
-                # Right line of the heightmap -> check to the left, top and bottom
-                if (
-                    digit[0] < heightmap[v_run_variable][h_index - 1][0]
-                    and digit[0] < heightmap[v_run_variable - 1][h_index][0]
-                    and digit[0] < heightmap[v_run_variable + 1][h_index][0]
-                    and digit[1] == False
-                ):
-                    sum_of_lowpoints += digit[0] + 1
-                    digit[1] = True
-                    debug.append((digit[0], h_index, v_run_variable))
-                    print(path)
-            elif h_index <= 0 and v_run_variable <= 0:
-                # Top left corner of the heightmap -> check to right and bottom
-                if (
-                    digit[0] < heightmap[v_run_variable][h_index + 1][0]
-                    and digit[0] < heightmap[v_run_variable + 1][h_index][0]
-                    and digit[1] == False
-                ):
-                    sum_of_lowpoints += digit[0] + 1
-                    digit[1] = True
-                    debug.append((digit[0], h_index, v_run_variable))
-                    print(path)
-            elif h_index >= len(line) - 1 and v_run_variable <= 0:
-                # Top right corner of the heightmap -> check to left and bottom
-                if (
-                    digit[0] < heightmap[v_run_variable][h_index - 1][0]
-                    and digit[0] < heightmap[v_run_variable + 1][h_index][0]
-                    and digit[1] == False
-                ):
-                    sum_of_lowpoints += digit[0] + 1
-                    digit[1] = True
-                    debug.append((digit[0], h_index, v_run_variable))
-                    print(path)
-            elif h_index <= 0 and v_run_variable >= len(heightmap) - 1:
-                # Bottom left corner of the heighmap -> check to right and top
-                if (
-                    digit[0] < heightmap[v_run_variable][h_index + 1][0]
-                    and digit[0] < heightmap[v_run_variable - 1][h_index][0]
-                    and digit[1] == False
-                ):
-                    sum_of_lowpoints += digit[0] + 1
-                    digit[1] = True
-                    debug.append((digit[0], h_index, v_run_variable))
-                    print(path)
-            elif h_index >= len(line) - 1 and v_run_variable >= len(heightmap) - 1:
-                # Bottom right corner of the heighmap -> check to left and top
-                if (
-                    digit[0] < heightmap[v_run_variable][h_index - 1][0]
-                    and digit[0] < heightmap[v_run_variable - 1][h_index][0]
-                    and digit[1] == False
-                ):
-                    sum_of_lowpoints += digit[0] + 1
-                    digit[1] = True
-                    debug.append((digit[0], h_index, v_run_variable))
-                    print(path)
+            # Marking all digits that do not have a smaller neighbor
+            # on the right side and/or are not a lowpoint
+            digit[1] = True
 
-    print(debug)
-    for line in heightmap:
-        print(line)
     return sum_of_lowpoints
 
 

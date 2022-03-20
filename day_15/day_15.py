f37a2b82f8a067600f7a0a0341c0da1105962b45
Atix95 <michael.weide08@gmail.com>
2022-03-20 21:59:54 +0100

    example solved with dijkstra

--- day_15/day_15.py
+++ day_15/day_15.py
@@ -1,59 +1,64 @@
 import os
+from collections import defaultdict
 
 
 #  Directions to check for adjacent risk levels
-DIRECTIONS = [(0, 1), (1, 0)]
+DIRECTIONS = [(0, 1), (1, 0), (-1, 0), (0, -1)]
 
 
 def load_risk_levels(file_name):
-    # Create a list of each digit in the risk levels. Because the risk levels are 
-    # given in one line as a coherent number, this number must be split in digits.
-    split_num_into_digits = lambda num: [int(digit) for digit in str(num)]
-
+    # Create a list of each digit in the risk levels
     file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), file_name)
+
     with open(file_path, "r", encoding="utf-8") as file_risk:
         risk_levels = [
-            split_num_into_digits(line)
-            for line in file_risk.read().split("\n")
+            [int(risk_level) for risk_level in line.strip()] for line in file_risk
         ]
 
     return risk_levels
 
 
 def find_path_with_lowest_risk(risk_levels):
-    possible_paths = [[[0,0]]]
-    path = []
-    paths = []
-    total_risk = 0
-    total_risks = []
     x_max = len(risk_levels[0]) - 1
     y_max = len(risk_levels) - 1
-    
-    while len(possible_paths) > 0:
-        path = [risk_level for risk_level in possible_paths[0]]
-        x = path[-1][0]
-        y = path[-1][1]
+    destination = (x_max, y_max)
+
+    node_risks = defaultdict(lambda: float("inf"))
+    node_risks[(0, 0)] = 0
+
+    nodes_to_visit = []
+    visited_notes = []
+    nodes_to_visit.append((0, 0))
+
+    while len(nodes_to_visit) > 0:
+        risks_nodes_to_visit = []
+
+        for nodes in nodes_to_visit:
+            risks_nodes_to_visit.append(node_risks[nodes])
+
+        current_risk = min(risks_nodes_to_visit)
+        current_node = nodes_to_visit[risks_nodes_to_visit.index(current_risk)]
+        x, y = current_node
 
         for dx, dy in DIRECTIONS:
-            if 0 <= x + dx <= x_max and 0 <= y + dy <= y_max and [x+dx, y+dy] not in path and (x+dx, y+dy) != (x_max, y_max):
-                print(path + [[x+dx,y+dy]])
-                possible_paths.append(path + [[x+dx, y+dy]])
-            elif (x+dx, y+dy) == (x_max, y_max):
-                print(paths+ [[x+dx,y+dy]])
-                paths.append(path + [[x+dx,y+dy]])
+            if (
+                0 <= x + dx <= x_max
+                and 0 <= y + dy <= y_max
+                and (x + dx, y + dy) not in visited_notes
+            ):
+                nodes_to_visit.append((x + dx, y + dy))
+                if (
+                    current_risk + risk_levels[y + dy][x + dx]
+                    < node_risks[(x + dx, y + dy)]
+                ):
+                    node_risks[(x + dx, y + dy)] = (
+                        current_risk + risk_levels[y + dy][x + dx]
+                    )
 
-        possible_paths = possible_paths[1:]
+        visited_notes.append((x, y))
+        nodes_to_visit.remove((x, y))
 
-    for path in paths:
-        total_risk = 0
-        print("path: ", path)
-        for x,y in path:
-            print(x,y)
-            total_risk += risk_levels[y][x]
-        
-        total_risks.append(total_risk-risk_levels[0][0])
-
-    return min(total_risks)
+    return node_risks[destination]
 
 
 if __name__ == "__main__":

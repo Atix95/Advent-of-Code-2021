74b47d57231bc925850372fe8693766b0bde140b
Atix95 <michael.weide08@gmail.com>
2022-03-21 00:28:00 +0100

    day 15 solved part 1 with dijkstra

--- day_15/day_15.py
+++ day_15/day_15.py
@@ -18,16 +18,37 @@ def load_risk_levels(file_name):
     return risk_levels
 
 
+def neighbours(position, visited_nodes, nodes_to_visit, risk_levels):
+    x, y = position
+
+    for dx, dy in DIRECTIONS:
+        if (
+            0 <= x + dx <= x_max(risk_levels)
+            and 0 <= y + dy <= y_max(risk_levels)
+            and (x + dx, y + dy) not in visited_nodes
+            and (x + dx, y + dy) not in nodes_to_visit
+        ):
+            yield (x + dx, y + dy)
+
+    return []
+
+
+def x_max(risk_levels):
+    return len(risk_levels[0]) - 1
+
+
+def y_max(risk_levels):
+    return len(risk_levels) - 1
+
+
 def find_path_with_lowest_risk(risk_levels):
-    x_max = len(risk_levels[0]) - 1
-    y_max = len(risk_levels) - 1
-    destination = (x_max, y_max)
+    destination = (x_max(risk_levels), y_max(risk_levels))
 
     node_risks = defaultdict(lambda: float("inf"))
     node_risks[(0, 0)] = 0
 
     nodes_to_visit = []
-    visited_notes = []
+    visited_nodes = []
     nodes_to_visit.append((0, 0))
 
     while len(nodes_to_visit) > 0:
@@ -37,34 +58,31 @@ def find_path_with_lowest_risk(risk_levels):
             risks_nodes_to_visit.append(node_risks[nodes])
 
         current_risk = min(risks_nodes_to_visit)
-        current_node = nodes_to_visit[risks_nodes_to_visit.index(current_risk)]
-        x, y = current_node
+        x, y = nodes_to_visit[risks_nodes_to_visit.index(current_risk)]
 
-        for dx, dy in DIRECTIONS:
+        if (x, y) == destination:
+            break
+
+        for neighbour_pos in neighbours(
+            (x, y), visited_nodes, nodes_to_visit, risk_levels
+        ):
+            nodes_to_visit.append(neighbour_pos)
             if (
-                0 <= x + dx <= x_max
-                and 0 <= y + dy <= y_max
-                and (x + dx, y + dy) not in visited_notes
+                current_risk + risk_levels[neighbour_pos[1]][neighbour_pos[0]]
+                < node_risks[neighbour_pos]
             ):
-                nodes_to_visit.append((x + dx, y + dy))
-                if (
-                    current_risk + risk_levels[y + dy][x + dx]
-                    < node_risks[(x + dx, y + dy)]
-                ):
-                    node_risks[(x + dx, y + dy)] = (
-                        current_risk + risk_levels[y + dy][x + dx]
-                    )
+                node_risks[neighbour_pos] = (
+                    current_risk + risk_levels[neighbour_pos[1]][neighbour_pos[0]]
+                )
 
-        visited_notes.append((x, y))
+        visited_nodes.append((x, y))
         nodes_to_visit.remove((x, y))
 
     return node_risks[destination]
 
 
 if __name__ == "__main__":
-    risk_levels = load_risk_levels("day_15_example_input.txt")
-    for line in risk_levels:
-        print(line)
+    risk_levels = load_risk_levels("day_15_input.txt")
 
     lowest_risk = find_path_with_lowest_risk(risk_levels)
     print(lowest_risk)

138ef31de9ffe77170cd08c35812fa50ace21211
Atix95 <michael.weide08@gmail.com>
2022-05-14 22:54:52 +0200

    set up until reduce()

--- day_18/day_18.py
+++ day_18/day_18.py
@@ -22,26 +22,48 @@ class Node:
         self.right = None
 
     def __str__(self):
-        if isinstance(self.value, int):
+        if self.is_leaf():
             return str(self.value)
-        return f"[{str(self.left)},{str(self.right)}]"
+        return f"[{str(self.left)}, {str(self.right)}]"
 
     def is_leaf(self) -> bool:
         return isinstance(self.value, int)
 
+    def is_branch(self) -> bool:
+        return isinstance(self, Node)
+
     def flatten(self) -> List[int]:
+        values = []
+
         if self.is_leaf():
             return [self.value]
 
-        values = []
         values.extend(self.left.flatten())
         values.extend(self.right.flatten())
 
         return values
 
     def depth(self) -> int:
-        # Conitnue
+        if self.is_leaf():
+            return 0
+        return max(self.left.depth() + 1, self.right.depth() + 1)
+
+    def depth_branches(self) -> int:
         return
+
+    def add_trees(self, tree_to_be_added):
+        original_tree = self
+
+        root = Node(None)
+        root.left = original_tree
+        root.right = tree_to_be_added
+
+        return root
+
+    def magnitude(self) -> int:
+        if self.is_leaf():
+            return self.value
+        return 3 * self.left.magnitude() + 2 * self.right.magnitude()
 
     @classmethod
     def build_binary_tree(cls, snail_number):
@@ -55,6 +77,27 @@ class Node:
         root.right = cls.build_binary_tree(snail_number[1])
 
         return root
+
+    def get_all_leafs(self):
+        list_of_leafs = []
+
+        if self.is_leaf():
+            return [self]
+
+        if self.left != None:
+            list_of_leafs.extend(self.left.get_all_leafs())
+        if self.right != None:
+            list_of_leafs.extend(self.right.get_all_leafs())
+
+        return list_of_leafs
+
+    def reduce(self):
+        # Check for explosions
+        if self.depth() > 4:
+            leafs = self.get_all_leafs()
+            return
+
+        return
 
 
 def check_if_snail_number_is_nested(x, y, x_depth=0, y_depth=0):
@@ -105,8 +148,11 @@ if __name__ == "__main__":
 
     # [[[[[9, 8], 1], 2], 3], 4]
     # [7, [6, [5, [4, [3, 2]]]]]
-    x = Node.build_binary_tree([7, [6, [5, [4, [3, 2]]]]])
-    print(x.flatten())
+    # x = Node.build_binary_tree([7, [6, [5, [4, [3, 2]]]]])
+    x = Node.build_binary_tree(9)
+    print(x)
+    print(x.depth())
+    print(x.get_all_leafs())
 
     """
         1. input: Knoten mit einer Zahl

cde43a6cd16f9df5571ee89f29d8a655c5fdabda
Atix95 <michael.weide08@gmail.com>
2022-07-19 00:04:12 +0200

    day 18 part 1 solved

--- day_18/day_18.py
+++ day_18/day_18.py
@@ -15,12 +15,9 @@ def load_snail_numbers(file_name):
     return snail_numbers
 
 
-def pairwise(iterable):
-    x = iter(iterable)
-    return zip(x, x)
-
-
 class BinaryNode:
+    # Solve the homework assignment using the structure of a binary tree
+
     def __init__(self, value) -> None:
         self.value = value
         self.parent = None
@@ -28,6 +25,7 @@ class BinaryNode:
         self.right = None
 
     def __str__(self) -> str:
+        # Visualize the binary tree as snailfish numbers
         if self.is_leaf():
             return str(self.value)
         return f"[{str(self.left)}, {str(self.right)}]"
@@ -48,6 +46,7 @@ class BinaryNode:
 
     @classmethod
     def build_binary_tree(cls, snail_number):
+        # Build the binary tree from a snail number
         root = cls(None)
 
         if isinstance(snail_number, int):
@@ -80,7 +79,9 @@ class BinaryNode:
         self.right = right_tree
         self.right.parent = self
 
-    def add_trees(self, tree_to_be_added):
+    def add_tree(self, tree_to_be_added):
+        # Create a new tree to which the two trees that are to
+        # be added are added as the left and right BinaryNode.
         left_tree = self
         right_tree = tree_to_be_added
 
@@ -91,16 +92,19 @@ class BinaryNode:
         return root
 
     def depth(self) -> int:
+        # Return the depth of the entire tree
         if self.is_leaf():
             return 0
         return max(self.left.depth() + 1, self.right.depth() + 1)
 
     def depth_of_branch(self) -> int:
+        # Return the depth of the current branch/leaf
         if self.parent == None:
             return 0
         return self.parent.depth_of_branch() + 1
 
     def get_all_leafs(self):
+        # Get all leafs and return them in a list
         list_of_leafs = []
 
         if self.is_leaf():
@@ -113,54 +117,111 @@ class BinaryNode:
 
         return list_of_leafs
 
-    def get_all_exploading_leafs(self) -> List[Tuple]:
+    def get_two_leftmost_exploading_leafs_incl_idx(self) -> List[Tuple]:
+        # Return the two leftmost exploading leafs as pairs
+        # with the respective position in list_of_leafs.
         exploading_leafs = []
 
         for leaf_idx, exploading_leaf in enumerate(self.get_all_leafs()):
-            if exploading_leaf.depth_of_branch() > 4:
-                exploading_leafs.append((leaf_idx, exploading_leaf))
+            if exploading_leaf.depth_of_branch() > 4 and len(exploading_leafs) < 4:
+                exploading_leafs.extend([leaf_idx, exploading_leaf])
 
         return exploading_leafs
 
+    def reset_parent_after_explosion(self):
+        # Set the left and right BinaryNodes of the parent BinaryNode
+        # to None as well as its value to 0 after an explosion occured.
+        self.parent.left = None
+        self.parent.right = None
+        self.parent.value = 0
+
     def explosion(self):
-        exploading_leafs = self.get_all_exploading_leafs()
+        # Explode the leftmost pair. First, get the leafs of the exploding pair with
+        # the respective position in the list_of_leafs. Then add the values to the value
+        # to the left or right of it, if they exit. The value of the parent BinaryNode is
+        # then set to 0 and its left and right BinaryNode are set to None.
+        (
+            left_idx,
+            left_leaf,
+            right_idx,
+            right_leaf,
+        ) = self.get_two_leftmost_exploading_leafs_incl_idx()
+
         list_of_leafs = self.get_all_leafs()
 
-        for left_leaf_pair, right_leaf_pair in pairwise(exploading_leafs):
-            left_idx, left_leaf = left_leaf_pair
-            right_idx, right_leaf = right_leaf_pair
+        if left_idx - 1 >= 0:
+            list_of_leafs[left_idx - 1].value += left_leaf.value
+        if right_idx <= len(list_of_leafs) - 2:
+            list_of_leafs[right_idx + 1].value += right_leaf.value
 
-            if left_idx - 1 >= 0:
-                list_of_leafs[left_idx - 1].value += left_leaf.value
-            if right_idx <= len(list_of_leafs) - 2:
-                list_of_leafs[right_idx + 1].value += right_leaf.value
+        left_leaf.reset_parent_after_explosion()
 
-            left_leaf.parent.left = None
-            left_leaf.parent.right = None
-            left_leaf.parent.value = 0
+    def get_splitting_leaf(self):
+        # Get the leftmost value in the list_of_leafs that is to be splitted
+        splitting_leaf = None
+
+        for leaf in self.get_all_leafs():
+            if leaf.value >= 10:
+                splitting_leaf = leaf
+                break
+
+        return splitting_leaf
+
+    def split(self):
+        # Split the leftmost value in the list_of_leafs. A new BinaryNode is created
+        # for the left and right BinaryNode of the BinaryNode that is to be splitted
+        # (splitting_leaf). The value of the left BinaryNode is rounded down and the
+        # value of the right BinaryNode is rounded up. Lastly, the value of the
+        # BinaryNode that was splitted is set to None.
+        splitting_leaf = self.get_splitting_leaf()
+
+        splitting_leaf.add_left_node(BinaryNode(splitting_leaf.value // 2))
+        splitting_leaf.add_right_node(BinaryNode((splitting_leaf.value + 1) // 2))
+        splitting_leaf.value = None
 
     def reduce(self):
-        # Check for explosions
-        if self.depth() > 4:
-            self.explosion()
+        # Reduce the snailfish number. An explosion is performed until all of them
+        # are resolved. Then the splitting of the snailfish numbers is performed.
+        # If an exploding pair is produced as a result of a split, the pair will
+        # be exploded before further splits are executed. The approach with a while
+        # loop is chosen, as a recursive call of reduce() leads to exceeding the
+        # maximum recursive calls.
+        while True:
+
+            if self.depth() > 4:
+                self.explosion()
+                continue
+
+            if self.get_splitting_leaf() != None:
+                self.split()
+                continue
+
+            break
 
         return self
 
     def magnitude(self) -> int:
+        # Calculate the magnitude of a snailnumber. The magnitude of a pair is the
+        # 3 times the magnitude of the elemnt plus 2 times the magnitude of the right
+        # element. If the element is a leaf the magnitude is just its value.
         if self.is_leaf():
             return self.value
         return 3 * self.left.magnitude() + 2 * self.right.magnitude()
 
 
+def main():
+    # Call all the necessary functions to do the homework assignment
+    snail_numbers = load_snail_numbers("day_18_input.txt")
+
+    sum = BinaryNode.build_binary_tree(snail_numbers.pop(0))
+
+    for snail_number in snail_numbers:
+        sum = sum.add_tree(BinaryNode.build_binary_tree(snail_number))
+        sum.reduce()
+
+    print(f"The final sum is: {sum}")
+    print(f"The magnitude of the final sum is: {sum.magnitude()}")
+
+
 if __name__ == "__main__":
-    snail_numbers = load_snail_numbers("day_18_example_input.txt")
-    # print("\n".join(str(line) for line in snail_numbers))
-    # print("Snail numbers added:")
-    # print(add_snail_numbers([7, [6, [5, [4, [3, 2]]]]]))
-    # print(Node.build_binary_tree(snail_numbers[0]))
-
-    # [[[[[9, 8], 1], 2], 3], 4]
-    # [7, [6, [5, [4, [3, 2]]]]]
-    x = BinaryNode.build_binary_tree([[3, [2, [1, [7, 3]]]], [6, [5, [4, [3, 2]]]]])
-    print(x)
-
+    main()
